087f6ba6282fa34a44d2d1b705b5defa7d0398e4
Balkaran <balkaranjaswal@gmail.com>
2020-09-08 21:19:38 -0600

    not working tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);  
+  const [error, setError] = useState("");
 
   const reset = function() {
     setName("");
@@ -18,7 +19,18 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
-
+  
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    props.onSave(name, interviewer);
+  }
+  
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -30,8 +42,10 @@ export default function Form(props) {
             placeholder="Enter Name Here"
             value={name}
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
